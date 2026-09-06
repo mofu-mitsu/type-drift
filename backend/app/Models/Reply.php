c181534b2e38dde0a3c076cbdc27aebf019b1d0a
もふみつ <mitsuki.momoka@i.softbank.jp>
2026-09-06 22:09:46 +0900

    Update Reply.php

--- backend/app/Models/Reply.php
+++ backend/app/Models/Reply.php
@@ -8,5 +8,20 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Reply extends Model
 {
     use SoftDeletes;
-    protected $fillable = ['bottle_id', 'user_id', 'body'];
+    protected $fillable = ['bottle_id', 'user_id', 'parent_reply_id', 'body'];
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_reply_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_reply_id');
+    }
+
+    public function reactions()
+    {
+        return $this->hasMany(ReplyReaction::class);
+    }
 }
